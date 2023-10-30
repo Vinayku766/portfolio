8210d28c708fe46f70d03fddc69cd4a7d1cd3296
Vinay <vinaykumaryadav766@gmail.com>
2023-10-30 16:05:53 +0530

    completed portfolio

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Front-end Developer</span>
+                    <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span>
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                  Hello, I have graduated from GGSIPU (Guru Govind Singh Indraprasta University) with B.voc (Software Development)
+                  During My college Years I have learnt languages like C, C++, Java, JavaScript as well as some other topics databases 
+                  like SQL (Structural Query Language), NoSql (MongoDB), etc. 
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                  After that I know about web development then I start to 
+                  learn basic designing of web pages with HTML, CSS, JavaScript I had a good hand on JavaScript then I started created 
+                  projects too small games like dice-game, simple calculator with HTML, CSS and JS.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                 After Learning those basic designing and building small projects I learn about basic fundamental approach.
+                 then I starts to learning ReactJs Due to its features like reusing components and library it provides to make smoother our journey.
+                 And I am looking forward in this coding journey. 
+                </Div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/data.js
@@ -0,0 +1,83 @@
+import p1 from "./assets/p-1.png";
+import p2 from "./assets/p-2.png";
+import p3 from "./assets/p-3.png";
+import p4 from "./assets/p-4.png";
+import p5 from "./assets/p-5.png";
+import p6 from "./assets/p-6.png";
+import p7 from "./assets/p-7.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Dashboard",
+        id: "dashboard",
+    },
+    {
+        name: "Website",
+        id: "website",
+    },
+];
+
+export const projects = [
+    {
+        name: "Beautiful Calculator saves time",
+        image: p1,
+        tags: [ "website"],
+    },
+    {
+        name: "Weather App to check weather updates",
+        image: p2,
+        tags: [ "website"],
+    },
+    {
+        name: "A Keeper App store all your Information",
+        image: p3,
+        tags: ["website"],
+    },
+    {
+        name: "Mapty App for travelling in city",
+        image: p4,
+        tags: ["website"],
+    },
+    {
+        name: "A Bank Application dashboard to join more users",
+        image: p5,
+        tags: ["dashboard"],
+    },
+    {
+        name: "ChatGpt-3 Build Something Amazing",
+        image: p6,
+        tags: ["dashboard", "Website"],
+    },
+    {
+        name: "Cinema App for Movie Search",
+        image: p7,
+        tags: ["website"],
+    },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 25,
+        description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 2,
+        description: "YEARS OF<br />EXPERIENCE",
+    },
+    {
+        id: 3,
+        year: 15,
+        description: "TECHNOLOGIES",
+    },
+    {
+        id: 4,
+        year: 20,
+        description: "COMMUNITY<br />POSTS",
+    },
+];
